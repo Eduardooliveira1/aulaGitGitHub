0b1288119d32911e796bf1e079206566ed177b94
Eduardo inacio <eduardoi10@yahoo.com.br>
2017-11-07 16:06:19 -0200

    Terceiro commit do projeto: apresentar o resultado da soma dos dois

--- arq.js
+++ arq.js
@@ -6,18 +6,7 @@ n2 = document.getElementById ("segundoNumero").value;
 n1 = parseInt (n1);
 n2 = parseInt (n2);
 soma = n1 + n2;
-}
-
-
-/* Atualizar para (4)
-function soma () {
-var n1, n2, soma;
-n1 = document.getElementById ("primeiroNumero").value;
-n2 = document.getElementById ("segundoNumero").value;
-n1 = parseInt (n1);
-n2 = parseInt (n2);
-soma = n1 + n2;
 document.getElementById ("resposta").innerHTML = soma;
 }
-*/
+
 
